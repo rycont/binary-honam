78b000985ff20542c7815e79bcc9343f4288b24a
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-07-31 07:46:30 +0000

    feat: support mobile viewport

--- src/service/cached-fetch/index.ts
+++ src/service/cached-fetch/index.ts
@@ -1,24 +1,24 @@
-const cache = new Map<string, Response>();
+const cache = new Map<string, Response>()
 
 export function cachedFetch(
     input: RequestInfo,
     init?: RequestInit
 ): Promise<Response> {
-    const key = JSON.stringify({ input, init });
+    const key = JSON.stringify({ input, init })
 
     if (cache.has(key)) {
-        console.log("Cache hit");
-        return Promise.resolve(cache.get(key)!.clone());
+        console.log('Cache hit')
+        return Promise.resolve(cache.get(key)!.clone())
     }
 
     return fetch(input, init).then((response) => {
-        console.log("Cache miss");
-        cache.set(key, response.clone());
-        return response;
-    });
+        console.log('Cache miss')
+        cache.set(key, response.clone())
+        return response
+    })
 }
 
 setInterval(() => {
-    cache.clear();
-    console.log('Cache cleared');
-}, 1000 * 60);
+    cache.clear()
+    console.log('Cache cleared')
+}, 1000 * 60)
