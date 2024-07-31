86c6cb5ab715e5ee30da70e04fce33e98b64eb00
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-07-31 07:52:17 +0000

    fix: sort train list by departure time

--- src/service/train-timetable/index.ts
+++ src/service/train-timetable/index.ts
@@ -6,31 +6,31 @@ import srt from './srt.json'
 import { TrainName, type TrainPlan, type TrainPlanQueryFilter } from './type'
 
 const trainKorNameToEnumMap: Record<string, TrainName> = {
-    '무궁화': TrainName.MUGUNGHWA,
+    무궁화: TrainName.MUGUNGHWA,
     'ITX-마음': TrainName.ITX_MAUM,
     'ITX-새마을': TrainName.ITX_SAEMAEUL,
     'KTX-산천': TrainName.KTX_SANCHEON,
     'KTX-청룡': TrainName.KTX_CHUNGRYONG,
-    'KTX': TrainName.KTX,
-    'SRT': TrainName.SRT,
+    KTX: TrainName.KTX,
+    SRT: TrainName.SRT,
 }
 
-const allTrainPlans: TrainPlan[] = [
-    ...korailExpress,
-    ...korailNormal,
-    ...srt,
-].map((trainPlan) => ({
-    ...trainPlan,
-    departure: {
-        station: trainPlan.departure.station,
-        time: parsePlanTime(trainPlan.departure.time),
-    },
-    arrival: {
-        station: trainPlan.arrival.station,
-        time: parsePlanTime(trainPlan.arrival.time),
-    },
-    trainName: trainKorNameToEnumMap[trainPlan.trainName],
-}))
+const allTrainPlans: TrainPlan[] = [...korailExpress, ...korailNormal, ...srt]
+    .map((trainPlan) => ({
+        ...trainPlan,
+        departure: {
+            station: trainPlan.departure.station,
+            time: parsePlanTime(trainPlan.departure.time),
+        },
+        arrival: {
+            station: trainPlan.arrival.station,
+            time: parsePlanTime(trainPlan.arrival.time),
+        },
+        trainName: trainKorNameToEnumMap[trainPlan.trainName],
+    }))
+    .toSorted((a, b) =>
+        Temporal.PlainTime.compare(a.departure.time, b.departure.time)
+    )
 
 const trainTimetable = {
     getTrainPlans(filter: Partial<TrainPlanQueryFilter>) {
