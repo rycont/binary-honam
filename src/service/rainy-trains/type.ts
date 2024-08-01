582cce50a8bd3e4f927395e0f0d563e7a1f3b881
정한 Rycont <35295182+rycont@users.noreply.github.com>
2024-08-01 13:53:47 +0000

    refactor: remove unused imports

--- src/service/rainy-trains/type.ts
+++ src/service/rainy-trains/type.ts
@@ -1,5 +1,4 @@
-import type { Temporal } from '@js-temporal/polyfill'
-import type { RainPlan, RainPlanWithTime } from '../rainy-plan/type'
+import type { RainPlanWithTime } from '../rainy-plan/type'
 import type { TrainPlan } from '../train-timetable/type'
 
 export interface RainyTrain {
